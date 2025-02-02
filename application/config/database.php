4ba33e507f78b9113c7fc24b22c74f6630133d89
Fuad Rivai <fuad.strummer@gmail.com>
2025-02-02 23:51:24 +0700

    modified gitignore

---
+++ application/config/database.php
@@ -0,0 +1,60 @@
+<?php
+/*
+-- ---------------------------------------------------------------
+-- MYRAPORT K13
+-- CREATED BY : NGODING PINTAR
+-- COPYRIGHT  : Copyright (c) 2019 - 2020, (youtube.com/ngodingpintar)
+-- CREATED ON : 2019-11-26
+-- UPDATED ON : 2020-02-10
+-- ---------------------------------------------------------------
+*/
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+$active_group = 'default';
+$query_builder = TRUE;
+
+$db['default'] = array(
+	'dsn'	=> '',
+	'hostname' => 'localhost',
+	'username' => 'root',
+	'password' => '',
+	'database' => 'ereport_secondary_bangka',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
+
+
+$db['schoolDb'] = array(
+	'dsn'	=> '',
+	'hostname' => 'localhost',
+	'username' => 'school_bangka',
+	'password' => 'd3dPLDab5ZS4twdH',
+	'database' => 'school_bangka',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
